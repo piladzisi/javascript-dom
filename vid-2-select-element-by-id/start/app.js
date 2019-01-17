b31da86529a409fce900ce87efd5fb0b5f7b2bce
Anna <piladzisi@gmail.com>
2019-01-17 19:22:30 +0000

    getElementsByTagName()[]

--- vid-2-select-element-by-id/start/app.js
+++ vid-2-select-element-by-id/start/app.js
@@ -1,8 +1,14 @@
-const myHeading = document.getElementById('myHeading');
+const myHeading = document.getElementsByTagName('h1')[0];
 const myInput = document.getElementById('myInput');
 const myButton = document.getElementById('myButton');
+const myList = document.getElementsByTagName ('li');
+
+
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myInput.value;
   myButton.style.backgroundColor = myInput.value;
+  for (let i=0; i< myList.length; i +=1){
+	myList[i].style.color = 'purple';
+}
 });
