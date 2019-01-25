4c827359f5b70189d0c00116245a65861bf8bf45
Anna <piladzisi@gmail.com>
2019-01-25 18:02:15 +0000

    Love this toggle button

--- vid-2-select-element-by-id/start/app2.js
+++ vid-2-select-element-by-id/start/app2.js
@@ -7,8 +7,10 @@ const li = document.querySelector('li:last-child');
 
 toggleList.addEventListener ('click', () => {
 	if(listDiv.style.display =='none') {
+	toggleList.textContent = 'Hide list';
 	listDiv.style.display = 'block';
 } else{
+	toggleList.textContent = 'Show list';
 	listDiv.style.display = 'none';
 }
 });
