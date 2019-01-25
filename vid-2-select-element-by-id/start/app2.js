a4e039ed36342889770e4f908a9599b7b54f421a
Anna <piladzisi@gmail.com>
2019-01-25 17:56:36 +0000

    inlinestyle with JS

--- vid-2-select-element-by-id/start/app2.js
+++ vid-2-select-element-by-id/start/app2.js
@@ -1,8 +1,16 @@
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
 const input = document.querySelector('input');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
 const li = document.querySelector('li:last-child');
 
-button.addEventListener('click', () => {
-	li.innerHTML = input.value 
+toggleList.addEventListener ('click', () => {
+	listDiv.style.display = 'none';
 });
+
+button.addEventListener('click', () => {
+	li.innerHTML = input.value ;
+});
+
+p.title = 'List Description'
