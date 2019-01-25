7624002ead5de1bf275084c164b727511403a44e
Anna <piladzisi@gmail.com>
2019-01-25 17:59:43 +0000

    Toggle button on/off using JS inline styles

--- vid-2-select-element-by-id/start/app2.js
+++ vid-2-select-element-by-id/start/app2.js
@@ -6,7 +6,11 @@ const button = document.querySelector('button.description');
 const li = document.querySelector('li:last-child');
 
 toggleList.addEventListener ('click', () => {
+	if(listDiv.style.display =='none') {
+	listDiv.style.display = 'block';
+} else{
 	listDiv.style.display = 'none';
+}
 });
 
 button.addEventListener('click', () => {
